6372f0bda45550951d7b83bdf1f1388987ff4a27
Malcolm McQueen <malcolmmcqueen@live.com>
2022-09-01 02:39:35 +0000

    added vertical and backwards searches to wordsearch function. updated tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,34 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'H', 'J', 'B', 'F', 'K', 'Q', 'D'],
+      ['E', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['F', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], "SEINFELD");
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], "DLEFNIES");
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (word === undefined) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let tempArr = [];
+  let verticalArr = [];
+  for (let i = 0; i < letters[0].length; i ++) {
+    for (let j = 0; j < letters.length; j ++) {
+      tempArr.push(letters[j][i]);
     }
-}
+    verticalArr.push(tempArr);
+    tempArr = [];
+  }
+  const verticalJoin = verticalArr.map(l => l.join(""));
+  for (let v of verticalJoin) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  let backTempArr = [];
+  let backArr = [];
+  for (let l of letters) {
+    for (let i = l.length - 1; i >= 0; i--) {
+      backTempArr.push(l[i]);
+    }
+    backArr.push(backTempArr);
+    backTempArr = [];
+  }
+  const backWardsJoin = backArr.map(l => l.join(""));
+  for (let b of backWardsJoin) {
+    if (b.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = wordSearch
